a670cce491d384b9dd9ee12bafea8db924d49184
t0ti20 <t0ti20@icloud.com>
2023-02-02 17:17:07 +0200

    Adding AVR

---
+++ Build/avr/lib/gcc/avr/12.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/Ubuntu/Desktop/GitHub/Tool_Chain/crosstool-ng/.build/avr/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=avr --prefix=/home/Ubuntu/x-tools/avr --exec_prefix=/home/Ubuntu/x-tools/avr --with-local-prefix=/home/Ubuntu/x-tools/avr/avr --with-headers=/home/Ubuntu/x-tools/avr/avr/include --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG UNKNOWN' --disable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libstdcxx-verbose --with-gmp=/home/Ubuntu/Desktop/GitHub/Tool_Chain/crosstool-ng/.build/avr/buildtools --with-mpfr=/home/Ubuntu/Desktop/GitHub/Tool_Chain/crosstool-ng/.build/avr/buildtools --with-mpc=/home/Ubuntu/Desktop/GitHub/Tool_Chain/crosstool-ng/.build/avr/buildtools --with-isl=/home/Ubuntu/Desktop/GitHub/Tool_Chain/crosstool-ng/.build/avr/buildtools --enable-lto --enable-target-optspace --disable-nls --enable-multiarch --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
